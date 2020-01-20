58363b4001446b0ac196d401626ef864aee14974
gr0v3r <g.cq143@gmail.com>
2020-01-19 23:39:00 -0400

    thre commit book app 2

---
+++ .dart_tool/flutter_build/b65eab6b56743e5fac5e71bf32089bf4/armeabi-v7a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Applications/flutter/flutter_1.3.8/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot
---
+++ .dart_tool/flutter_build/b65eab6b56743e5fac5e71bf32089bf4/x86_64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Applications/flutter/flutter_1.3.8/bin/cache/artifacts/engine/android-x64-release/darwin-x64/gen_snapshot
